71e6aa690068518adc70136a80f9221d4c690a53
chris <chris@123.com>
2023-03-16 10:08:56 +0800

    2023-3-16完成模型部分的修改

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -14,6 +14,7 @@ class UNet(nn.Module):
         self.down1 = (Down(64, 128))
         self.down2 = (Down(128, 256))
         self.down3 = (Down(256, 512))
+        # 双线性插值增大对图像进行上采样
         factor = 2 if bilinear else 1
         self.down4 = (Down(512, 1024 // factor))
         self.up1 = (Up(1024, 512 // factor, bilinear))
@@ -21,6 +22,12 @@ class UNet(nn.Module):
         self.up3 = (Up(256, 128 // factor, bilinear))
         self.up4 = (Up(128, 64, bilinear))
         self.outc = (OutConv(64, n_classes))
+        # 构建滤波器分支
+        self.filter_up1 = Up(1024, 512 // factor, bilinear)
+        self.filter_up2 = Up(512, 256 // factor, bilinear)
+        self.filter_up3 = Up(256, 128 // factor, bilinear)
+        self.filter_up4 = Up(128, 64 // factor, bilinear)
+        # self.filter_out = OutConv(64)
 
     def forward(self, x):
         x1 = self.inc(x)
